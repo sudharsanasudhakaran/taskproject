5e44860440150dc8d87426841203c4c693214b82
Sudhakaran <SUDHARSANA.SUDHAKARAN@Chubb.com>
2020-07-10 12:35:17 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { DayComponent } from './day/day.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     DayComponent
   ],
   imports: [
--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayComponent implements OnInit {
   days =[]
+  tasks =[]
   constructor() { }
 
   ngOnInit(): void {
   }
 adddays(){
-  let i =0;
-    i = this.days[this.days.length-1];
-    this.days.push(++i)
+  this.days.push({
+    id : this.days.length +1,
+    name: `Day ${this.days.length + 1}`
+  })
 }
+addtask()
+{
+let j=0;
+  j = this.tasks[this.tasks.length-1];
+  this.tasks.push(++j)
+}
+
 }
